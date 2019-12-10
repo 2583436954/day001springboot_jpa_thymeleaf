8324eb6100d9bb6be01a16c2125c17507cf5fc11
wmc <2583436954@qq.com>
2019-12-10 20:51:01 +0800

    保护好我的代码

---
+++ src/main/java/com/wmc/springboot_jpa_thymeleaf/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.wmc.springboot_jpa_thymeleaf.dao;
+
+import com.wmc.springboot_jpa_thymeleaf.bean.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String name);
+    User findByNameAndAge(String name,Integer age);
+}
---
+++ src/test/java/com/wmc/springboot_jpa_thymeleaf/SpringbootJpaThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmc.springboot_jpa_thymeleaf;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
